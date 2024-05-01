54915c36e04439d8f6a25b2afb1216febe5c5a28
Vadym Pavlyk <71404100+Made1ra@users.noreply.github.com>
2024-05-01 19:56:40 +0300

    Create schema.ts

---
+++ src/db/schema.ts
@@ -0,0 +1,26 @@
+import {
+    pgTable,
+    serial,
+    text,
+    integer,
+} from 'drizzle-orm/pg-core';
+
+export const users = pgTable('users', {
+    id: serial('id').primaryKey(),
+    name: text('name').notNull(),
+    email: text('email').notNull(),
+    image: text('image').notNull(),
+});
+
+export const ratings = pgTable('ratings', {
+    id: serial('id').primaryKey(),
+    userID: text('user_id').references(() => users.id).notNull(),
+    movieID: text('movie_id').notNull(),
+    rating: integer('rating').notNull(),
+});
+
+export const watchlist = pgTable('watchlist', {
+    id: serial('id').primaryKey(),
+    userID: text('user_id').references(() => users.id).notNull(),
+    movieID: text('movie_id').notNull(),
+});
