2f0a4d440d650c2c26bac2a3791368a66b5aff13
Vadym Pavlyk <71404100+Made1ra@users.noreply.github.com>
2024-05-03 20:01:11 +0300

    Update home.routes.ts

--- src/routes/home.routes.ts
+++ src/routes/home.routes.ts
@@ -1,16 +1,27 @@
 import express from 'express';
-import { postUser } from '../queries';
+import { postUser, getUser } from '../queries';
 
 const router = express.Router();
 
 router.post('/', async (request, response) => {
-    const { name, email, image } = request.body;
+    const { id, name, email, image } = request.body;
     try {
-        await postUser(name, email, image);
+        await postUser(id, name, email, image);
         response.status(201).json({ message: 'User created successfully' });
     } catch (error) {
         console.error('Error creating user:', error);
         response.status(500).json({ error: 'An error occurred while creating the user' });
+    }
+});
+
+router.get('/', async (request, response) => {
+    const { id } = request.body;
+    try {
+        const user = await getUser(id);
+        response.status(200).json(user);
+    } catch (error) {
+        console.error('Error getting the user:', error);
+        response.status(500).json({ error: 'An error occurred while getting the user' });
     }
 });
 
