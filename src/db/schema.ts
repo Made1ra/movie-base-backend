23c3eb0b0e2c72e2586659fce987d1b369021cc0
Vadym Pavlyk <71404100+Made1ra@users.noreply.github.com>
2024-05-03 20:00:51 +0300

    Update schema.ts

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -6,7 +6,7 @@ import {
 } from 'drizzle-orm/pg-core';
 
 export const users = pgTable('users', {
-    id: serial('id').primaryKey(),
+    id: text('id').primaryKey(),
     name: text('name').notNull(),
     email: text('email').notNull(),
     image: text('image').notNull(),
