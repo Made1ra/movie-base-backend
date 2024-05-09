6a77265390d4e148dba5cb03f5dd17d2a925df21
Vadym Pavlyk <71404100+Made1ra@users.noreply.github.com>
2024-05-09 15:22:53 +0300

    Fix bug with sending json response twice

--- src/routes/title.routes.ts
+++ src/routes/title.routes.ts
@@ -9,7 +9,7 @@ router.get('/title/:id', async (request, response) => {
     try {
         const rating = await getRating(userID, id);
         const watchlist = await getMovieFromWatchlist(userID, id);
-        response.status(200).json(rating).json(watchlist);
+        response.status(200).json({ rating, watchlist });
     } catch (error) {
         console.error('Error getting the ratings:', error);
         response.status(500).json({ error: 'An error occurred while getting the ratings' });
